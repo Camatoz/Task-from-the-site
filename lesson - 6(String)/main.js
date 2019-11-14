48aed78cc6c231265ca2ad4eb13cb9ab956e3003
Camatoz <yurabondik99@gmail.com>
2019-11-14 20:16:08 +0300

    Work with String.

--- lesson - 6(String)/main.js
+++ lesson - 6(String)/main.js
@@ -2,6 +2,20 @@ function clog(text) {
     console.log(text);
 }
 
-// Task - 1 ()
-var str = 'Мне очень нравится JavaScript';
-document.write(str.substr(-10, 4));
+// Task - 1 (Дана строка 'js'. Сделайте из нее строку 'JS'.)
+let a = 'js';
+clog(a.toUpperCase());
+
+// Task - 2 (Дана строка 'JS'. Сделайте из нее строку 'js'.)
+let b = 'JS';
+clog(b.toLowerCase());
+
+// Task - 3 (Дана строка 'я учу javascript!'. Найдите количество символов в этой строке.)
+let str = 'я учу javascript!';
+clog(str.length);
+
+// Task - 4 (Дана строка 'я учу javascript!'. Вырежите из нее слово 'учу' и слово 'javascript' тремя разными способами (через substr, substring, slice).)
+str = 'я учу javascript!';
+clog(str.substr(2, 14));
+clog(str.substring(2, 16));
+clog(str.slice(2, 16));
