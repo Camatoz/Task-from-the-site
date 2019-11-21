afc6f3df60c91b3069b31a61be7fadc156f51685
Camatoz <yurabondik99@gmail.com>
2019-11-21 11:01:43 +0300

    Work with array.

--- lesson - 12(Array)/main.js
+++ lesson - 12(Array)/main.js
@@ -1,46 +1,77 @@
 function clog(text) {
-	console.log(text)
+  console.log(text);
 }
 
 // Task - 1 (Заполните массив следующим образом: в первый элемент запишите 'x', во второй 'xx', в третий 'xxx' и так далее.)
 let arr = [];
-let str = 'x';
+let str = "x";
 for (let i = 0; i < 5; i++) {
-    arr[i] = str;
-    str += 'x';
+  arr[i] = str;
+  str += "x";
 }
 clog(arr);
 
 // Task - 2 (Заполните массив следующим образом: в первый элемент запишите '1', во второй '22', в третий '333' и так далее.)
-str = '';
+str = "";
 arr = [];
-for (let i = 1; i < 5; i++) { 
-    for (let j = 1; j <= i; j++) {
-        str += i;
-    }
-    arr.push(str);
-    str = '';
+for (let i = 1; i < 5; i++) {
+  for (let j = 1; j <= i; j++) {
+    str += i;
+  }
+  arr.push(str);
+  str = "";
 }
 clog(arr);
 
 // Task - 3 (Сделайте функцию arrayFill, которая будет заполнять массив заданными значениями. Первым параметром функция принимает значение, которым заполнять массив, а вторым - сколько элементов должно быть в массиве. Пример: arrayFill('x', 5) сделает массив ['x', 'x', 'x', 'x', 'x'].)
 let emptyArr = [];
 function arrayFill(value, length) {
-    var arr = [];
-	for (var i = 0; i < length; i++) {
-		arr.push(value);
-	}
-	return arr;
+  var arr = [];
+  for (var i = 0; i < length; i++) {
+    arr.push(value);
+  }
+  return arr;
 }
-clog(arrayFill('x', 5));
+clog(arrayFill("x", 5));
 
 // Task - 4 (Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.)
 arr = [1, 3, 6, 7, 3, 45, 2];
 let a = 0;
-for(let i = 0; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
   a += arr[i];
-  if(a > 10) {
+  if (a > 10) {
     console.log(i);
     break;
   }
 }
+
+// Task -5 (Дан массив с числами. Не используя метода reverse переверните его элементы в обратном порядке.)
+arr = [1, 2, 3, 4];
+arr2 = [];
+for (let i = arr.length - 1; i >= 0; i--) {
+  arr2.push(arr[i]);
+}
+
+clog(arr2);
+
+// Task - 6 (Дан двухмерный массив с числами, например [[1, 2, 3], [4, 5], [6]]. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.)
+let arr3 = [[1, 2, 3], [4, 5], [6]];
+let sum = 0;
+for (var i = 0; i < arr3.length; i++) {
+  for (var j = 0; j < arr3[i].length; j++) {
+    sum += arr3[i][j];
+  }
+}
+clog(sum);
+
+// Task - 7 (Дан трехмерный массив с числами, например [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.)
+let arr4 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+let sum4 = 0;
+for ( let i = 0; i < arr4.length; i++) {
+  for (let j = 0; j < arr4[i].length; j++) {
+    for (let k = 0; k < arr4[i][j].length; k++) {
+      sum4 += arr4[i][j][k];
+    }
+  }
+}
+clog(sum4);
