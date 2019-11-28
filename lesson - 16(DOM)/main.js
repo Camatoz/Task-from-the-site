46444efe114484dff17492d2f086e1da16edde97
Camatoz <yurabondik99@gmail.com>
2019-11-28 17:31:35 +0300

    Useduse DOM

--- lesson - 16(DOM)/main.js
+++ lesson - 16(DOM)/main.js
@@ -2,4 +2,21 @@ function clog(text) {
     console.log(text);
 }
 
-clog(555);
+// Task - 1 (Повторите страницу по данному по образцу)
+function buttonClick() {
+	alert('Hi, there');
+}
+
+// Task - 2 (Повторите страницу по данному по образцу)
+function buttonClick1() {
+	alert('Hi, there');
+}
+
+// Task - 3 (Повторите страницу по данному по образцу)
+function buttonClick2() {
+	alert('Hi, there');
+}
+
+
+
+
